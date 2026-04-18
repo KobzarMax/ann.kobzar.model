499a2e47b67d00b496ed4471916a252ee7d89867
KobzarMax <nimbus2219@gmail.com>
2026-04-18 18:05:06 +0100

    feat: add contact page with inquiry form and integrate into header navigation

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,23 @@
+import { getPhotos } from '@/api';
+import AboutImage from '@/components/AboutImage';
+import ContactForm from '@/components/ContactForm';
+import { ROUTE_PORTFOLIO } from '@/routes/routes';
+import Link from 'next/link';
+
+export default async function About() {
+  const photos = await getPhotos();
+
+  return (
+    <main className="bg-white lg:overflow-hidden lg:h-[calc(100dvh-84px)] lg:max-h-[calc(100dvh-84px)]">
+      <div className="grid grid-rows-[auto_1fr] lg:grid-rows-1 lg:grid-cols-2 md:gap-10 gap-4 py-3 px-5 lg:p-2.5 h-full">
+        <div className="lg:max-h-[calc(100dvh-84px)] h-full flex flex-col items-center lg:justify-center md:gap-10 gap-4 py-3 lg:py-10">
+          <ContactForm />
+        </div>
+        <div className="h-full flex items-center justify-center appearBlock overflow-hidden relative">
+          <AboutImage photos={photos} />
+          <Link href={ROUTE_PORTFOLIO} className="absolute inset-0 lg:hidden" />
+        </div>
+      </div>
+    </main>
+  );
+}
